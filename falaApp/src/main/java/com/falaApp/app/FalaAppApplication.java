dec85484eac9e2ce710154dba11b9239e4bab773
javacodepoint <ravikumarverma53@gmail.com>
2020-06-24 14:39:55 +0530

    first commit

---
+++ falaApp/src/main/java/com/falaApp/app/FalaAppApplication.java
@@ -0,0 +1,13 @@
+package com.falaApp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalaAppApplication.class, args);
+	}
+
+}
